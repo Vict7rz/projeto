458836be9b45ccfbd95bb1a05a2fb81b32b0c5cc
Victor Moura <vm164216@gmail.com>
2023-07-03 20:30:20 -0300

    leaving it my way

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   //substituir a img
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     //se tiver sem light mode, manter a imagem dark
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
